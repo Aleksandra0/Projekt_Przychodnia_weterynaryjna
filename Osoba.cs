8ffbbaab5e92b6ba7323a35e3f9bcdb26f9e10dc
Aleksandra <aleksandra.borkowska@interia.eu>
2021-01-02 18:52:38 +0100

    Stworzenie klasy Osoba

---
+++ Osoba.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_Przychodnia_weterynaryjna
+{
+    abstract class Osoba
+    {
+        public enum Plcie { K, M, I } //I - other
+
+        private string imie;
+        private string nazwisko;
+        private DateTime data_urodzenia;
+        private string pesel;
+        private Plcie plec;
+
+        public string Imie { get => imie; set => imie = value; }
+        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+        public DateTime Data_urodzenia { get => data_urodzenia; set => data_urodzenia = value; }
+        public string Pesel { get => pesel; set => pesel = value; }
+        public Plcie Plec { get => plec; set => plec = value; }
+
+        public Osoba()
+        {
+
+        }
+
+        public Osoba(string imie, string nazwisko, DateTime data_urodzenia, string pesel, Plcie plec)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.data_urodzenia = data_urodzenia;
+            this.pesel = pesel;
+            this.plec = plec;
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ namespace Projekt_Przychodnia_weterynaryjna
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
